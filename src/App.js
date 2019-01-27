0c78cd98eca383bdcc7ce3d80ef88784b6e79afe
David Arango <arango.david8@gmail.com>
2019-01-26 20:27:42 -0500

    Added reset css file in public folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
